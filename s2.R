f8150ba14133546a94edb05a4bb1866c90a00137
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-04-11 14:08:19 +0200

    maj

--- s2.R
+++ s2.R
@@ -18,15 +18,58 @@ oison_geom <- sf::read_sf(dsn = "oison/oison_geometry.gpkg")
 
 # mise en forme des données ----
 
-## sélection de champs ----
+## traitements de champs ----
 
 oison_taxon %>%
 dplyr::select(nom_vernaculaire, nom_scientifique, date, nom)
 
-## sélection de tous les champs sans certains
+### sélection de tous les champs sans certains
 
 oison_taxon %>%
   dplyr::select(!(c(nom_vernaculaire, nom_scientifique, date, nom)))
+
+### sélection des champs selon leur début/fin (<> ^/debut, $/fin)
+
+oison_taxon %>%
+  dplyr::select(starts_with("nom"))
+
+oison_taxon %>%
+  dplyr::select(ends_with("id"))
+
+### sélection des champs selon la présence d'une valeur (ou d'une autre)
+
+oison_taxon %>%
+  dplyr::select(contains("barrage") | contains("Barrage"))
+
+### réorganiser les champs (commencer par ceux démarrant pr id puis les autres champs)
+
+oison_taxon %>%
+  dplyr::select(start_with("id"), everything())
+
+### créer de nouveaux champs
+
+test2 <- oison_taxon %>% 
+  dplyr::select(observation_id, nom:email) %>% 
+  mutate(nom_complet_observateur = paste(nom, prenom, sep = '_'),
+         addition = 1 + 10,
+         racine_carree = sqrt(observation_id))
+
+## traitement observations
+
+### selection d'observation
+
+oison_taxon %>% 
+  dplyr::filter(nom_scientifique == 'Canis lupus') %>% 
+  dplyr::select(observtion_id:nom_vernaculaire)
+
+### selection d'observation bis
+
+oison_taxon %>% 
+  dplyr::filter(email %in% c('camille.riviere@ofb.gouv.fr', 'didier.pujot@ofb.gouv.fr')) %>% 
+  dplyr::select(observation_id::presence)
+
+test <- oison_taxon %>% 
+  dplyr::filter(presence == 'Absent' & !is.na(nombre_individu))
 
 # export des données ----
 
